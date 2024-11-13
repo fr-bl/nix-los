cd8a91b407179b24d0705ad09edda7fcce15e010
Frederick <hey@blaes.ing>
2024-11-13 13:10:52 +0100

    fix script not found

--- get.sh
+++ get.sh
@@ -2,5 +2,4 @@
 SOURCE_DIR=./nix-los
 git clone --depth 1 https://github.com/fr-bl/nix-los "$SOURCE_DIR"
 
-chmod +x "$SOURCE_DIR"/*.sh
-./"$SOURCE_DIR"/shell.sh
+(cd "$SOURCE_DIR" && chmod +x ./*.sh && ./shell.sh)
