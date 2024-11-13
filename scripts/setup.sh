8c8a3454a6232c7e5c8bbf1ea2d067732a71eb00
Frederick Bläsing <hey@blaes.ing>
2024-11-13 15:11:11 +0100

    format setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-resolve_url() {
+resolve-url() {
 	curl -w "%{url_effective}\n" -I -L -s -S "$1" -o /dev/null;
 }
 
-source ./vars.sh
-if [ ! -f "$NIXPATH" ]; then
-	NIXURL=$(resolve_url $NIXURL)
+install-nix() {
+	NIXURL=$(resolve-url $NIXURL)
 	mkdir -p "$NIXDIR"
 	curl --output "$NIXPATH" "$NIXURL"
 	chmod +x "$NIXPATH"
-fi
+}
+
+source ./vars.sh
+if [ ! -f "$NIXPATH" ]; then install-nix; fi
 export PATH="$PATH:$NIXDIR"
--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-source ./install.sh
+source ./setup.sh
 $SHELL
